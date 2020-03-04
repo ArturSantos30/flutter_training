3947e892d0cdea6a225c134896e605d7a0e424a9
Artur Santos <artursantos30.ars@gmail.com>
2020-03-04 01:18:38 -0400

    Contador de pessoas implementado

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -3,7 +3,40 @@ import 'package:flutter/material.dart';
 void main() {
     runApp(MaterialApp(
         title: "Contador de Pessoas",
-        home: Stack(
+        home: Home()
+    ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people = 0;
+  String _infoText = "Pode entrar!!!";
+
+  void _changePeople( int delta){
+    setState(() {
+      _people += delta;
+
+      if( _people < 0){
+        _infoText = "Mundo invertido?";
+      }
+      else if( _people <= 10){
+        _infoText = "Pode entrar!";
+      }
+      else{
+        _infoText = "Lotado!";
+      }
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
           children: <Widget>[
             Image.asset(
               "images/restaurante.jpg",
@@ -13,7 +46,7 @@ void main() {
             Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-                Text("Pessoas: 0", 
+                Text("Pessoas: $_people", 
                 style: TextStyle(
                     color: Colors.white, 
                     fontWeight: FontWeight.bold)
@@ -25,7 +58,9 @@ void main() {
                       padding: EdgeInsets.all(10.0),
                       child: FlatButton(
                         child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                        onPressed: () {}, 
+                        onPressed: () {
+                          _changePeople(1);
+                        }, 
                       ),
                     ),
 
@@ -33,13 +68,15 @@ void main() {
                       padding: EdgeInsets.all(10.0),
                       child: FlatButton(
                         child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                        onPressed: () {}, 
+                        onPressed: () {
+                          _changePeople(-1);
+                        }, 
                       ),
                     ),
                     
                   ],
                 ),
-                Text("Pode entrar", 
+                Text( _infoText, 
                 style: TextStyle(
                     color: Colors.white,
                     fontStyle: FontStyle.italic, 
@@ -48,6 +85,6 @@ void main() {
             ],
             )
           ] 
-        )
-    ));
+        );
+  }
 }
